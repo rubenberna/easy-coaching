c8bdb52d8cca450e3c1ef4cc52327abe913846a0
rubenberna <rubenmbernardes@gmail.com>
2020-02-11 11:49:10 +0100

    feat(): added more info with links to email

--- server/sendEmail/index.js
+++ server/sendEmail/index.js
@@ -13,6 +13,20 @@ const taskCreated = (task) => {
       <p><span><b>Description:</b></span> ${task.description}</p>
       <p><span><b>Start:</b></span> ${moment(task.start).format('MMMM Do YYYY, h:mm a')}</p>
       <p><span><b>End:</b></span> ${moment(task.end).format('MMMM Do YYYY, h:mm a')}</p>
+      <br />
+      <p>Client details:</p>
+      <hr />
+      <p><span><b>Name:</b></span> ${task.client.Name}</p>
+      <p><span><b>Email:</b></span> <a href="mailto:${task.client.Email}" target="_blank">${task.client.Email}</a></p>
+      <p><span><b>Phone:</b></span> <a href="tel: ${task.client.Phone}">${task.client.Phone}</a></p>
+      <p><span><b>Address:</b></span> <a href="https://maps.google.com/?q=${task.client.MailingAddress.street}, ${task.client.MailingAddress.city}" target="_blank">${task.client.MailingAddress.street}, ${task.client.MailingAddress.city}</a></p>
+      <p><span><b>Office:</b></span> <a href="https://maps.google.com/?q=${task.client.Account.BillingAddress.street}, ${task.client.Account.BillingAddress.city}" target="_blank">${task.client.Account.Name}</a></p>
+      <br />
+      <p>HouseKeeper details:</p>
+      <hr />
+      <p><span><b>Name:</b></span> ${task.houseKeeper.Name}</p>
+      <p><span><b>Email:</b></span> <a href="mailto:${task.houseKeeper.Email}" target="_blank">${task.houseKeeper.Email}</a></p>
+      <p><span><b>Phone:</b></span> <a href="tel: ${task.houseKeeper.Phone}">${task.houseKeeper.Phone}</a></p>
       <p>Thanks!</p>
       <p>Coaching team</p>
     `
@@ -91,6 +105,7 @@ const sendMsg = (msg) => {
   const message = {
     from: '"Coaching team" <coaching@easylifedc.be>',
     to: `<${msg.body.to}>`,
+    cc: `<${msg.body.from}>`,
     attachments: attach,
     subject: `New msg from ${msg.body.from}` ,
     html: `<p>Hi,</p>
